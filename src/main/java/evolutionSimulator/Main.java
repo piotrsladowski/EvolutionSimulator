c9db7c52bcb2bd9cbe43e51abac4443ca09b77d2
piotrsladowski <piotrsladowski7@gmail.com>
2019-12-05 11:23:13 +0100

    Dla Marcina

--- src/main/java/evolutionSimulator/Main.java
+++ src/main/java/evolutionSimulator/Main.java
@@ -1,7 +1,6 @@
 package evolutionSimulator;
 
 import evolutionSimulator.ConfigFile.ConfigFile;
-import evolutionSimulator.Controllers.LoadSettings;
 import evolutionSimulator.View.MainWindow;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -10,9 +9,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ConfigFile configFile = new ConfigFile(getClass().getResource("/config.test").getFile());
-        configFile.load();
-        int gridSize = Integer.parseInt(configFile.getGeneralProperties().get("gridSize"));
+        //ConfigFile configFile = new ConfigFile(getClass().getResource("/config.test").getFile());
+        //configFile.load();
+        //int gridSize = Integer.parseInt(configFile.getGeneralProperties().get("gridSize"));
+        int gridSize = 20;
         MainWindow mainWindow = new MainWindow(primaryStage, gridSize);
         mainWindow.build(10, 10);
     }
